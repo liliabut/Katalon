0a9c8d6d9913d37cb1db47952a4ec902716d6c4d
vr-test <vr-test@UNITB-DELL-14.Werk.UnitB.de>
2018-04-04 16:23:39 +0200

    GAA und Akk. Anpassungen

--- Test Suites/Akk.ts
+++ Test Suites/Akk.ts
@@ -4,6 +4,7 @@
    <name>Akk</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-04-04T15:46:02</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
