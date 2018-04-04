8a4e4ddcba01bf17d1116a114a308d87dae34540
vr-test <vr-test@UNITB-DELL-14.Werk.UnitB.de>
2018-04-04 12:37:40 +0200

    Neue TestCases für Akk. und GAA

---
+++ Scripts/Prototyp_GAA_Suche (RC 18-2-0)/20_Testfall/Script1522835526482.groovy
@@ -0,0 +1,106 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.deleteAllCookies()
+
+WebUI.navigateToUrl('http://rc-ogrhtmlprototyp.testintern/18-1-0/geldautomatensuche-bundesweit.html')
+
+WebUI.delay(5)
+
+WebUI.setText(findTestObject('13GAA/Page_Geldautomatensuche-bundesweit/input_city'), 'Berlin')
+
+WebUI.click(findTestObject('13GAA/Page_Geldautomatensuche-bundesweit/button_Suchen'))
+
+WebUI.delay(5)
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/01ErgebnisText'), 'Geldautomat: Berliner Volksbank eG, FinanzCenter Neukölln')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/02ErgebnisText'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/03ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/04ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/05ErgebnisTest'), 'Geldautomat: Sparda-Bank Berlin eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/06ErgebnisTest'), 'Geldautomat: BBBank eG, Filiale')
+
+WebUI.click(findTestObject('naechsteSeite'))
+
+WebUI.delay(2)
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/07ErgebnisTest'), 'Geldautomat: Sparda-Bank Berlin eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/08ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/09ErgebnisTest'), 'Geldautomat: Sparda-Bank Berlin eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/10ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/11ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/12ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.click(findTestObject('naechsteSeite'))
+
+WebUI.delay(2)
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/13ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/14ErgebnisTest'), 'Geldautomat: Sparda-Bank Berlin eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/15ErgebnisTest'), 'Geldautomat: Sparda-Bank Berlin eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/16ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG, FinanzCenter Friedrichstraße')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/17ErgebnisTest'), 'Geldautomat: BBBank eG, SB Filiale, nicht öffentlich zugänglich')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/18ErgebnisTest'), 'Geldautomat: BBBank eG, Filiale')
+
+WebUI.click(findTestObject('naechsteSeite'))
+
+WebUI.delay(2)
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/19ErgebnisTest'), 'Geldautomat: GLS Gemeinschaftsbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/20ErgebnisTest'), 'Geldautomat: Deutsche Apotheker- und Ärztebank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/21ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/22ErgebnisTest'), 'Geldautomat: Evangelische Bank eG - Filiale Berlin -')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/23ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/24ErgebnisTest'), 'Geldautomat: Berliner Volksbank eG')
+
+WebUI.click(findTestObject('naechsteSeite'))
+
+WebUI.delay(2)
+
+WebUI.verifyElementText(findTestObject('OGRPrototypFilialsuche - Berlin Zentrum/25ErgebnisTest'), 'Geldautomat: Sparda-Bank Berlin eG')
+
+WebUI.closeBrowser()
+
