4e18b444a3613a4ce287e8b67a10e79505419c8b
vr-test <vr-test@UNITB-DELL-14.Werk.UnitB.de>
2018-04-09 17:10:39 +0200

    Nur erster Akkordeon offen

---
+++ Object Repository/Akkordion_Open.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Akkordion_Open</name>
+   <tag></tag>
+   <elementGuidId>55e86144-60fa-4bfc-b7ec-0d961b6a4bd7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@class=&quot;acc-header active&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/01_AkkordeonOffen/Script1523285558309.groovy
@@ -0,0 +1,55 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.exception.StepFailedException as StepFailedException
+import com.kms.katalon.core.webui.common.WebUiCommonHelper as WebUiCommonHelper
+import org.openqa.selenium.WebElement as WebElement
+
+WebUI.openBrowser('')
+
+WebUI.deleteAllCookies()
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl('http://rc-ogrhtmlprototyp.testintern/18-2-0/accordion-overview.html')
+
+WebUI.delay(5)
+
+ArrayList<WebElement> wes = WebUiCommonHelper.findWebElements(findTestObject('Akkordion_open'), 5)
+
+if(wes.size > 1) {
+	WebUI.comment("Mehr alz 1 Akkordion sind offen!")
+	WebUI.closeBrowser()
+	throw new StepFailedException("Mehr alz 1 Akkordion sind offen!")
+} else if(wes.size == 0) {
+	WebUI.comment("Kein Akkordion ist offen!")
+	WebUI.closeBrowser()
+	throw new StepFailedException("Kein Akkordion ist offen!")
+} else {
+	String text = wes.get(0).getAttribute('innerHTML')
+	
+	if (!text.equalsIgnoreCase("Akkordeon 1")) {
+		WebUI.comment("Nicht erster Akkordeon ist offen!")
+		WebUI.closeBrowser()
+		throw new StepFailedException("Nicht erster Akkordeon ist offen!")
+	}	
+}
+
+WebUI.closeBrowser()
