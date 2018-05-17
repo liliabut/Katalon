a89ae4ac3dd2d90a9f6c2b1f75849d6d732906aa
vr-test <vr-test@UNITB-DELL-14.Werk.UnitB.de>
2018-05-17 16:33:56 +0200

    Neue GAA Test-Cases angelegt

---
+++ Object Repository/loc_city_filialsuche_30.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>loc_city_filialsuche_30</name>
+   <tag></tag>
+   <elementGuidId>8480d5c1-9c24-41b3-b99a-71103c49d88a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='loc-city']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
